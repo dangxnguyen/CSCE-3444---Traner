33e2650edfd04f763fa842a555b32987da01821d
Dang Nguyen <xuanhaidang.nguyen@gmail.com>
2022-10-30 21:38:14 -0500

    modify url

--- home/urls.py
+++ home/urls.py
@@ -5,8 +5,9 @@ from django.urls import include, re_path
 app_name = 'home'
 
 urlpatterns = [
-    re_path(r'^$', views.login_view, name="login"),
-    re_path(r'home/login.html', views.login_view, name="login"),
+    re_path(r'^$', views.home, name="home"),
+    re_path(r'home/homepage.html', views.home, name="home"),
+    re_path(r'^login.html/$', views.login_view, name="login"),
     re_path(r'^options/$', views.options_view, name="options"),
     re_path(r'^compose/$', views.compose_view, name="compose"),
     re_path(r'options/compose.html', views.compose_view, name="compose"),
