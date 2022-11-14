c53fa33d7832fc8f8f9fba65af234d8976a4d1c6
Britney Perdomo <britney@Britneys-MBP.attlocal.net>
2022-11-14 17:53:17 -0600

    setting change to allow hosting

--- eVoice/settings.py
+++ eVoice/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&wbue#)2a!z0%5pjv!t_j%y+m-6&l6es4ndh=s3yc9u%w&^(p4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://traner.pythonanywhere.com"]
 
 
 # Application definition
