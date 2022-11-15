5dabdd5d047813d716d61a9b40155c3afa28f65f
Britney Perdomo <britney@Britneys-MBP.attlocal.net>
2022-11-14 21:59:05 -0600

    updated all designated pages to have logout functionality

--- eVoice/settings.py
+++ eVoice/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&wbue#)2a!z0%5pjv!t_j%y+m-6&l6es4ndh=s3yc9u%w&^(p4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
